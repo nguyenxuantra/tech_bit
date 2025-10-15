f50117ad879c238a279a86b1c748807b489f85b1
nguyenxuantra <nguyenxuantra2k3@gmail.com>
2025-10-15 17:01:23 +0700

    config render

--- src/main/java/com/tech_bit/tech_bit/configuration/SecurityConfig.java
+++ src/main/java/com/tech_bit/tech_bit/configuration/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://tech-bit.onrender.com"));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
