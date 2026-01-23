8500620fda479fae2988d5ad845e318b0ae88aa5
nguyenxuantra <nguyenxuantra@gmail.com>
2026-01-23 13:53:02 +0700

    code

--- src/main/java/com/tech_bit/tech_bit/dto/request/ACategoryRequest.java
+++ src/main/java/com/tech_bit/tech_bit/dto/request/ACategoryRequest.java
@@ -2,9 +2,10 @@ package com.tech_bit.tech_bit.dto.request;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
-@Builder
+@NoArgsConstructor
 public class ACategoryRequest {
     private String name;
     private String imageUrl;
